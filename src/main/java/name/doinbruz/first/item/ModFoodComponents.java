98e0ec387841310edca10816e13f0e55c2a04058
doinBruz <leigh.faulkner99@gmail.com>
2025-06-08 16:46:30 +1000

    changed sound and effects

--- src/main/java/name/doinbruz/first/item/ModFoodComponents.java
+++ src/main/java/name/doinbruz/first/item/ModFoodComponents.java
@@ -15,15 +15,14 @@ public class ModFoodComponents {
 
     public static final ConsumableComponent TRIPPA_SNIPPA_CONSUMABLE_COMPONENT = ConsumableComponents.food()
         .consumeEffect(new ApplyEffectsConsumeEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 20 * 15, 1), 1.0f))
-        .consumeEffect(new ApplyEffectsConsumeEffect(new StatusEffectInstance(StatusEffects.JUMP_BOOST, 20 * 60, 1), 1.0f))
-        .consumeEffect(new ApplyEffectsConsumeEffect(new StatusEffectInstance(StatusEffects.SPEED, 20 * 60, 1), 1.0f))
-        .consumeEffect(new ApplyEffectsConsumeEffect(new StatusEffectInstance(StatusEffects.HASTE, 20 * 60, 1), 1.0f))
-        .consumeEffect(new ApplyEffectsConsumeEffect(new StatusEffectInstance(StatusEffects.STRENGTH, 20 * 60, 1), 1.0f))
-        .consumeEffect(new ApplyEffectsConsumeEffect(new StatusEffectInstance(StatusEffects.GLOWING, 20 * 180, 1), 0.1f))
-        .consumeEffect(new ApplyEffectsConsumeEffect(new StatusEffectInstance(StatusEffects.DARKNESS, 20 * 5, 1), 1.0f))
-        .consumeEffect(new ApplyEffectsConsumeEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 20 * 5, 1), 1.0f))
-        .consumeEffect(new ApplyEffectsConsumeEffect(new StatusEffectInstance(StatusEffects.POISON, 20 * 5, 1), 1.0f))
-        .consumeEffect(new ApplyEffectsConsumeEffect(new StatusEffectInstance(StatusEffects.BLINDNESS, 20 * 5, 1), 1.0f))
+        .consumeEffect(new ApplyEffectsConsumeEffect(new StatusEffectInstance(StatusEffects.JUMP_BOOST, 20 * 90, 1), 1.0f))
+        .consumeEffect(new ApplyEffectsConsumeEffect(new StatusEffectInstance(StatusEffects.SPEED, 20 * 90, 1), 1.0f))
+        .consumeEffect(new ApplyEffectsConsumeEffect(new StatusEffectInstance(StatusEffects.HASTE, 20 * 90, 1), 1.0f))
+        .consumeEffect(new ApplyEffectsConsumeEffect(new StatusEffectInstance(StatusEffects.STRENGTH, 20 * 90, 1), 1.0f))
+        .consumeEffect(new ApplyEffectsConsumeEffect(new StatusEffectInstance(StatusEffects.DARKNESS, 20 * 15, 1), 1.0f))
+        .consumeEffect(new ApplyEffectsConsumeEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 20 * 15, 1), 1.0f))
+        .consumeEffect(new ApplyEffectsConsumeEffect(new StatusEffectInstance(StatusEffects.POISON, 20 * 15, 1), 1.0f))
+        .consumeEffect(new ApplyEffectsConsumeEffect(new StatusEffectInstance(StatusEffects.BLINDNESS, 20 * 15, 1), 1.0f))
         .build();        
 }
 
